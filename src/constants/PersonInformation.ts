7e56b19e4e786f5c62c9dbac7b0fbb4aada7a6e7
Carlos Gonzalez <justdevelopitmx@gmail.com>
2023-05-23 02:33:16 -0700

    React three fiber

--- src/constants/PersonInformation.ts
+++
@@ -1,5 +0,0 @@
-export class PersonInformation {
-  static readonly FIRST_NAME = "Carlos"
-  static readonly LAST_NAME = "Gonzalez"
-  static readonly FULL_NAME = `${this.FIRST_NAME} ${this.LAST_NAME}`
-}
